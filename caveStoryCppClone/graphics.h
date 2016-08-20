a1ba9dc870a17e2d89854f1d5cae0cd3422fddd4
Benjamin_Rybolt <corbin.phallus@gmail.com>
2016-08-19 19:51:54 -0500

    Finished today's work.

--- caveStoryCppClone/graphics.h
+++ caveStoryCppClone/graphics.h
@@ -1,6 +1,14 @@
 #ifndef GRAPHICS_H
 #define GRAPHICS_H
 
+/* Graphics class
+* Holds all information about the graphics in this game.
+*/
+
+#include <map>
+#include <string>
+#include <SDL_image.h>
+
 struct SDL_Window;
 struct SDL_Renderer;
 
@@ -8,10 +16,40 @@ class Graphics {
 public:
 	Graphics();
 	~Graphics();
+
+	/* SDL_Surface* loadImage
+	 * Loads an image into the _spriteSheets map if it doesn't already exist
+	 * As a result, each timage will only ever be loaded once
+	 * Returns the image fromt he map regardless of whether or not it was just loaded
+	 */
+	SDL_Surface* loadImage(const std::string &filePath);
+
+	/* void blitSurface
+	 * Drawrs a texture to a certain part of the screen
+	 */
+	void blitSurface(SDL_Texture* source, SDL_Rect* sourceRectangle, SDL_Rect* destinationRectangle);
+
+	/* void flip
+	 * Renders everything to the screen
+	 */
+	void flip();
+
+	/* void clear();
+	 * Clears the screen
+	 */
+	void clear();
+
+	/* SDL_Renderer* getRenderer
+	 * Returns the renderer
+	 */
+
+	SDL_Renderer* getRenderer() const;
+
 private:
 	SDL_Window* _window;
 	SDL_Renderer* _renderer;
 
+	std::map<std::string, SDL_Surface*>_spriteSheets;
 };
 
 #endif
